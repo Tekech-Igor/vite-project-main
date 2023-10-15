d1c8418f0a469bd79970bc5de4bf6b1f3f4ef7b0
Tekech-Igor <115574737+Tekech-Igor@users.noreply.github.com>
2023-10-15 17:27:09 +0400

    fixed errors

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,21 +1,20 @@
-<% - injectScript %>
-import { getPage } from "../../shared/lib/index.js";
+import { getPage } from "../../shared/lib"
 
 export default () => {
   return getPage({
     title: "О нас",
     body: `
-            <h1>О нас</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/">Главная</a>
-                    </li>
-                    <li>
-                        <a href="/about.html">О Нас</a>
-                    </li>
-                </ul>
-            </nav>
-        `
+      <h1>Главная</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Главная</a>
+          </li>
+          <li>
+            <a href="/about.html">О Нас</a>
+          </li>
+        </ul>
+      </nav>
+    `
   })
 }
